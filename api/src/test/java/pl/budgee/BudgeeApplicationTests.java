ba16f4898c47b9e6639caa823dc56816ecf5df59
dletkiewicz <jokerek99@gmail.com>
2023-12-01 18:37:42 +0100

    removed test

--- api/src/test/java/pl/budgee/BudgeeApplicationTests.java
+++ api/src/test/java/pl/budgee/BudgeeApplicationTests.java
@@ -1,13 +1,8 @@
 package pl.budgee;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BudgeeApplicationTests {
-
-  @Test
-  void contextLoads() {
-  }
 
 }
