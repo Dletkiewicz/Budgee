d7b6f52e85bc40d616f81a0643ce2af1a27ea28c
dletkiewicz <jokerek99@gmail.com>
2023-12-02 14:45:06 +0100

    added user entity parameters

--- api/src/main/java/pl/budgee/adapter/jpa/UserEntity.java
+++ api/src/main/java/pl/budgee/adapter/jpa/UserEntity.java
@@ -1,4 +1,27 @@
 package pl.budgee.adapter.jpa;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.NaturalId;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
 public class UserEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "id")
+  private UUID databaseId;
+
+  @NaturalId
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "business_id")
+  private UUID businessId;
+
+  @Column(name = "username")
+  private String username;
+
+  @Column(name = "password")
+  private String password;
 }
