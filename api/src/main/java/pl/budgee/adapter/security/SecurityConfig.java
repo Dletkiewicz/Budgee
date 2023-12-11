89336c95139c7e5d09d47c83f9959518f6e0ea7d
dletkiewicz <jokerek99@gmail.com>
2023-12-11 17:50:28 +0100

    added github oauth2 login

--- api/src/main/java/pl/budgee/adapter/security/SecurityConfig.java
+++ api/src/main/java/pl/budgee/adapter/security/SecurityConfig.java
@@ -2,40 +2,29 @@ package pl.budgee.adapter.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http.authorizeHttpRequests(auth -> {
+      auth.requestMatchers(
+          "/swagger-ui/**",
+          "/swagger-ui.html",
+          "/v3/api-docs",
+          "/v3/api-docs/**",
+          "/api/v3/api-docs/**"
+          ).permitAll();
+      auth.anyRequest().authenticated();
+    })
+        .oauth2Login(Customizer.withDefaults())
+        .formLogin(Customizer.withDefaults())
+        .build();
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
-      http.authorizeHttpRequests(requests -> {
-        requests
-            .requestMatchers(
-                mvc.pattern("/swagger-ui.html"),
-                mvc.pattern("/swagger-ui/**"),
-                mvc.pattern("/v3/api-docs"),
-                mvc.pattern("/v3/api-docs/**"),
-                mvc.pattern("/api/v3/api-docs/**")
-            )
-            .permitAll() // Permit all to OpenAPI
-            .anyRequest()
-            .authenticated();
-      });
-
-      return http.build();
-    }
-
-    @Scope("prototype")
-    @Bean
-    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
-      return new MvcRequestMatcher.Builder(introspector);
-    }
-
+  }
 }
